6ef5b3ddbf9d70930d8c73fc82c3bd30e4dc848d
yasmin3li <yasmin123@gmail.com>
2026-04-11 23:37:29 -0800

    feat: add query to verify employee ownership of complaint state transition

--- src/main/java/com/myapp/complaints/DAO/ComplaintTracingLogRepo.java
+++ src/main/java/com/myapp/complaints/DAO/ComplaintTracingLogRepo.java
@@ -1,12 +1,17 @@
 package com.myapp.complaints.DAO;
 
 import com.myapp.complaints.dto.ComplaintTrackingLogDto;
+import com.myapp.complaints.entity.Account;
 import com.myapp.complaints.entity.ComplaintTrackingLog;
+import com.myapp.complaints.enums.ComplaintState;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ComplaintTracingLogRepo extends JpaRepository<ComplaintTrackingLog,Long> {
 
     List<ComplaintTrackingLogDto> findByComplaintId(Long complaintId);
+
+    Optional<ComplaintTrackingLog> findByComplaint_IdAndActionBy_IdAndNewState(Long complaintId, long accountId, ComplaintState state);
 }
