fc118729c8297c8c89e1f69d3e594c380ed17e6a
yasmin3li <yasmin123@gmail.com>
2026-04-14 18:48:03 -0800

    feat: add new DTO for complaint update workflow with optional fields and images support [deleteOldImages]

---
+++ src/main/java/com/myapp/complaints/dto/UpdateComplaintDto.java
@@ -0,0 +1,26 @@
+package com.myapp.complaints.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+public record UpdateComplaintDto(
+
+        @NotNull
+        long complaintId,
+        String title,
+        String description,
+        Long serviceId,
+        Long governorateId,
+        Long institutionId,
+        Long sectorId,
+        Double latitude,
+        Double longitude,
+        @Size(max = 3)
+        List<String> images,
+
+        //by default is false
+        boolean deleteOldImages
+) {
+}
