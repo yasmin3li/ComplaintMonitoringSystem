80235a9256816991b8cca7cf3de762be7d99dd43
yasmin3li <yasmin123@gmail.com>
2026-06-12 01:33:01 -0800

    fix(EmployeePerformanceDto): add JsonIgnore annotation to comingCount field and add assigned field to return the current assigned number

--- src/main/java/com/myapp/complaints/dto/EmployeePerformanceDto.java
+++ src/main/java/com/myapp/complaints/dto/EmployeePerformanceDto.java
@@ -1,11 +1,16 @@
 package com.myapp.complaints.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.List;
 
 public record EmployeePerformanceDto(
 
         long employeeAccountId,
+
+        @JsonIgnore
         long comingCount,
+        long assigned,
         long handledCount,
         double responseRate,
         double score,
